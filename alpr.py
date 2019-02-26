ae1f79115c4c0b090d59d43a22b65493c197bfb0
MrDoctorKovacic <qcasey1@gmail.com>
2019-02-26 12:11:55 -0800

    Adding slack support and multiple plates per image

--- alpr.py
+++ alpr.py
@@ -1,18 +1,20 @@
 # Python 2.7
 from openalpr import Alpr
+from slackclient import SlackClient 
 import sys
+import argparse
 import sqlite3 # for both portable and I/O sensitive record keeping
 import json
 import os.path
 import urllib
 import time
 
-DEBUG_PRINT = True
-CREATE_NEW_DB = False
+DEBUGGING = True
+SC = None
+c = None
 
-if(not os.path.isfile('alprRecords.db')): CREATE_NEW_DB = True
-conn = sqlite3.connect('alprRecords.db')
-c = conn.cursor()
+def sendSlackMessage(slackChannel, message):
+    SC.api_call("chat.postMessage", channel=slackChannel, text=message)
 
 def createTable():
     # Create table
@@ -20,12 +22,24 @@ def createTable():
     conn.commit()
 
 def insertRow(time, records, camera):
-    c.execute("INSERT INTO alpr VALUES ('{}','{}',{})".format(time, records, camera))
+    c.execute("INSERT INTO alpr VALUES ('{}','{}',{})".format(time, records.replace('"', '""'), camera))
     conn.commit()
 
 if __name__ == "__main__":
-    # Create table if the file didn't exist
-    if(CREATE_NEW_DB): createTable()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--db', nargs=1, help="SQLite DB File to use")
+    parser.add_argument('--slack', nargs=2, help="Slack API token and channel")
+    args = parser.parse_args()
+
+    if(len(args.slack) == 2):
+        SC = SlackClient(args.slack[0])
+
+    # Create table if the DB file doesn't exist
+    DB_FILE = args.db[0] if args.db else 'alprRecords.db'
+    conn = sqlite3.connect(DB_FILE)
+    c = conn.cursor()
+
+    if(not os.path.isfile(DB_FILE)): createTable()
 
     # Set up image recognition
     alpr = Alpr("us", "/etc/openalpr/openalpr.conf", "/usr/share/openalpr/runtime_data/")
@@ -44,7 +58,7 @@ if __name__ == "__main__":
 
         # File to be saved (possibly temporarily) for alpr
         alprImage = "./images/camera{}-{}.jpg".format(currentCamera, currentTime)
-
+	
         # Save image from camera
         urllib.urlretrieve("http://localhost:808{}/?action=snapshot".format(currentCamera), alprImage)
 
@@ -54,10 +68,14 @@ if __name__ == "__main__":
 
         # Save if a match otherwise delete image
         if(results["results"]):
-            insertRow(currentTime, results_json, currentCamera)
+	    for plate in results["results"]:
+                insertRow(currentTime, json.dumps(plate), currentCamera)
 
-            if(DEBUG_PRINT):
-                print("Found plate: {}".format(results["results"][0]["plate"]))
+                if(DEBUGGING):
+		    message = "Found plate: {}".format(json.dumps(plate["plate"]))
+                    print(message)
+		    if(SC): sendSlackMessage(args.slack[1], message)
+
         else:
             os.remove(alprImage)
         
